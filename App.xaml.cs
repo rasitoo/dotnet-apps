5c832906b56cb0a1ee3b0b750e0e509a07f56b09
Rodrigo <05rotapi@gmail.com>
2024-12-18 13:30:24 +0100

    Agregar archivos de proyecto.

---
+++ App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace P05_01_DI_Productos_TAPIADOR_rodrigo
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
