bbe20cc7c3ee3bd7caa7f325266b7b34e1c9bdb7
Rodrigo <05rotapi@gmail.com>
2025-01-07 20:15:54 +0100

    Hecho con WPF-UI y con usercontrol propios

--- UI/Views/MainWindow.xaml.cs
+++ UI/Views/MainWindow.xaml.cs
@@ -1,11 +1,9 @@
-﻿using System.Windows;
-
-namespace P05_01_DI_Productos_TAPIADOR_rodrigo;
+﻿namespace P05_01_DI_Productos_TAPIADOR_rodrigo;
 
 /// <summary>
 /// Interaction logic for MainWindow.xaml
 /// </summary>
-public partial class MainWindow : Window
+public partial class MainWindow 
 {
     public MainWindow()
     {
