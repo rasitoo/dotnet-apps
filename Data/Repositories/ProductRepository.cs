fe652d20e87c6e6f421881be7367ff4f40cd2184
Rodrigo <05rotapi@gmail.com>
2025-01-27 13:15:16 +0100

    imagenes a productos

--- Data/Repositories/ProductRepository.cs
+++ Data/Repositories/ProductRepository.cs
@@ -6,7 +6,7 @@ namespace P06_01_DI_Contactos_TAPIADOR_rodrigo.Data.Repositories;
 internal class ProductRepository(AppDbContext context) : IRepository<Product>
 {
     private readonly AppDbContext _context = context;
-    public List<Product> GetAll() => [.. _context.Products];
+    public List<Product> GetAll() => _context.Products.ToList();
     public void Add(Product product) => _context.Add(product);
     public void Delete(Product product) { _context.Products.Remove(product).State = EntityState.Deleted; _context.SaveChanges(); }
     public Product? Get(int id) => _context.Products.Find(id);
