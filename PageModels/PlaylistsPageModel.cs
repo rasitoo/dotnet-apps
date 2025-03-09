c2060afd3ec837d5ba514dbd4144d0ac1d983bab
rasitoo <05rotapi@gmail.com>
2025-03-09 12:52:17 +0100

    show playlists and fix playlistpage

--- PageModels/PlaylistsPageModel.cs
+++ PageModels/PlaylistsPageModel.cs
@@ -1,12 +1,15 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
 using P07_01_DI_Contactos_TAPIADOR_rodrigo.Data.Entities;
+using P07_01_DI_Contactos_TAPIADOR_rodrigo.Data.Rest;
 using System.Collections.ObjectModel;
+using System.Threading.Tasks;
 
 namespace P07_01_DI_Contactos_TAPIADOR_rodrigo.PageModels;
 
 public partial class PlaylistsPageModel : ObservableObject
 {
+    private IRestClient<Playlist> _playlistRestClient;
     [ObservableProperty]
     private ObservableCollection<Playlist> _playlists = new();
     [ObservableProperty]
@@ -20,6 +23,23 @@ public partial class PlaylistsPageModel : ObservableObject
             await Shell.Current.GoToAsync($"playlist?id={value.Id}");
             SelectedPlaylist = null;
         }
+    }
+    public PlaylistsPageModel(IRestClient<Playlist> playlistRestClient)
+    {
+        _playlistRestClient = playlistRestClient;
+        LoadAsync();
+    }
+
+    private async Task LoadAsync()
+    {
+        List<Playlist> playlists = await _playlistRestClient.GetAll(0, 100);
+        LoadPlaylists(playlists);
+    }
+
+    private void LoadPlaylists(List<Playlist> playlists)
+    {
+        Playlists = new(playlists);
+        PlaylistsLoading = false;
     }
 
     [RelayCommand]
